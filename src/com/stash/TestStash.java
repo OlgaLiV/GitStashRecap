b9d23c9eef7d445e923275aa649ce74cc738de69
OlgaLiV <9755609@gmail.com>
2020-09-25 02:57:29 +0300

    TastStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -2,7 +2,8 @@ package com.stash;
 
 public class TestStash {
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("Modifiction of our code");
+		System.out.println("Hello World");
 	}
 
 }
